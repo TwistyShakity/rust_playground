0a41a1ddd789845367cf79c5c028145c0aae6b55
Joe <eatmynoodle@icloud.com>
2023-10-17 19:28:53 +0800

    Add print debug format helper function

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
-#[derive(Default, Debug)]
+use std::fmt::Debug;
+
+#[derive(Default, Debug, Clone)]
 struct SomeStruct {
     int_field: i32,
     bool_field: bool,
     string_field: String,
 }
 
+
+fn print_debug_format<T: Debug>(target: T) {
+    println!("{:#?}", target)
+}
 fn main() {
     let mut some_instance = SomeStruct {
         string_field: "Hello".into(),
         ..Default::default()
     };
 
-    {
-        let move_string = some_instance.string_field;
-    }
-    some_instance.string_field = "Bye".into();
-
-    println!("{:#?}", some_instance);
+    print_debug_format(some_instance);
 }
