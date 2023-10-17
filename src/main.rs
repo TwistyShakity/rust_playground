805a67f2f36473c086073d7b29a9322c8f3d9f8d
Joe <eatmynoodle@icloud.com>
2023-10-17 19:23:19 +0800

    Partial move behavior

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[derive(Default, Debug)]
+struct SomeStruct {
+    int_field: i32,
+    bool_field: bool,
+    string_field: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut some_instance = SomeStruct {
+        string_field: "Hello".into(),
+        ..Default::default()
+    };
+
+    {
+        let move_string = some_instance.string_field;
+    }
+    some_instance.string_field = "Bye".into();
+
+    println!("{:#?}", some_instance);
 }
